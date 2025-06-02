0c09aa4527ada24286bff66c2127a3079c29bffa
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-02 23:09:45 +0300

    Lazy git config init
    -Formatter on save (; and ' -> ")
    -LazyGit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,10 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = "LazyGit",
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Lazygit" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
