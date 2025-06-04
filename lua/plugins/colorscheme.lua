30429b050fe36aeadde51040067fbda8dead9a83
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-04 08:51:18 +0300

    Colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/sonokai" },
+  { "Mofiqul/vscode.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
