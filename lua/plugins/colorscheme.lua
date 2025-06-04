b6f35efca4aab5d1625bca2a88cdc2180950e1d5
Hasan Özçelik <hasan.ozcelik@expert-in-cloud.com>
2025-06-04 10:05:33 +0300

    Colorscheme transparent test

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,11 +4,24 @@ return {
   { "EdenEast/nightfox.nvim" },
   { "sainnhe/sonokai" },
   { "Mofiqul/vscode.nvim" },
-
+  { "Mofiqul/dracula.nvim" },
+  { "craftzdog/solarized-osaka.nvim" },
+  { "rose-pine/neovim" },
+  { "scottmckendry/cyberdream.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "tokyonight",
     },
   },
 }
