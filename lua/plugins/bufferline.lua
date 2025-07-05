54db761cd756f56abd62607a45cfe5db83a976bd
Hasan Özçelik <hasan.ozcelik@expert-in-cloud.com>
2025-07-05 10:03:30 +0300

    bufferline names

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,6 +9,9 @@ return {
     options = {
       show_buffer_close_icons = false,
       show_close_icon = false,
+      max_name_length = math.huge,
+      max_prefix_length = math.huge,
+      truncate_names = false,
     },
   },
 }
