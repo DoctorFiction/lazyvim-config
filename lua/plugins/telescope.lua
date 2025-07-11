6f8d002c9e44ed5bb248e37f9da5d098d7fef418
Hasan Özçelik <doctorfiction@hotmail.com>
2025-07-11 17:55:57 +0300

    feat: configure telescope to show full paths and remove logdown.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    local defaults = opts.defaults or {}
-    defaults.path_display = { "absolute" }
-    opts.defaults = defaults
-    return opts
-  end,
+  opts = {
+    defaults = {
+      path_display = { "absolute" },
+    },
+  },
 }
